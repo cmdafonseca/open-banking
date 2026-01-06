1dcccb766dcb409a55cba6560fe4547f44f2dd27
cristina <cristina-maria.dafonseca@sicpa.com>
2026-01-06 15:46:40 +0100

    Initial commit

---
+++ src/main/java/github/com/cmdafonseca/openbanking/OpenBankingApplication.java
@@ -0,0 +1,13 @@
+package github.com.cmdafonseca.openbanking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenBankingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpenBankingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/github/com/cmdafonseca/openbanking/OpenBankingApplicationTests.java
@@ -0,0 +1,13 @@
+package github.com.cmdafonseca.openbanking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenBankingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
